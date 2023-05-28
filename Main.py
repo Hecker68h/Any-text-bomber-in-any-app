c34eb0250d9f763761f967c3a2812aaf04bf991b
Hecker68h <126754766+Hecker68h@users.noreply.github.com>
2023-05-28 11:17:52 +0530

    Update Main.py
    No_one369
    2 months ago
    If you want to make a message bomber with python do this :-
    First create a folder and name it anything 
    then inside the folder create a file named main.py.
    then press Win + R and type:
    pip install pyautogui
    then open your favourite code editor and type :

--- Main.py
+++ Main.py
@@ -1,1 +1,9 @@
+import pyautogui
+import time
 
+time.sleep(3)
+count = 0
+while count <= 4:
+    pyautogui.typewrite("you can write anything here" + str(count))
+    pyautogui.press("enter")
+    count = count + 1
